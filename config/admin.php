ee1328b9e156cf402cdcc2e2d71d43dd8fbc56a3
h2ero <122750707@qq.com>
2018-03-14 13:06:01 +0800

    changed name

--- config/admin.php
+++ config/admin.php
@@ -5,17 +5,17 @@ return [
     /*
      * Laravel-admin name.
      */
-    'name' => 'Laravel-admin',
+    'name' => '订餐管理',
 
     /*
      * Logo in admin panel header.
      */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>订餐</b> 管理',
 
     /*
      * Mini-logo in admin panel header.
      */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>订</b>',
 
     /*
      * Route configuration.
