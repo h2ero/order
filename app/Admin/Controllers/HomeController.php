f508b0d9a3cddcda80eebac90f534b7460278ada
h2ero <122750707@qq.com>
2018-03-14 15:47:58 +0800

    changed admin home

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Widgets\InfoBox;
 
 class HomeController extends Controller
 {
@@ -15,10 +16,15 @@ class HomeController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('Dashboard');
-            $content->description('Description...');
 
-            $content->row(Dashboard::title());
+            $content->row(function ($row) use ($content) {
+                $content->row(function ($row) {
+                    $row->column(3, new InfoBox('员工管理', 'users', 'aqua', '/admin/users', \App\Models\User::count()));
+                    $row->column(3, new InfoBox('套餐管理', 'book', 'yellow', '/admin/product', \App\Models\Product::count()));
+                    $row->column(3, new InfoBox('订餐记录', 'book', 'yellow', '/admin/order', \App\Models\Order::count()));
+                    $row->column(3, new InfoBox('月度报表', 'file', 'red', '/admin/order/stats', \App\Models\Order::select(\DB::raw('count(distinct month) c'))->value('c')));
+                });
+            });
 
         });
     }
